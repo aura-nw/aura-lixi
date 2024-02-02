9958fd44514f1bca9083f877d3bee78d611b5502
imhson <phamhoangson99@gmail.com>
2024-02-02 16:49:00 +0700

    update seo

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,26 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: `Year of the Dragon’s Li Xi`,
   description: 'Use Fortune Code to get Li Xi, Share Fortune Code for more Li Xi',
+  metadataBase: new URL('https://lixi.aura.network'),
+  openGraph: {
+    title: `Year of the Dragon’s Li Xi`,
+    description: 'Use Fortune Code to get Li Xi, Share Fortune Code for more Li Xi',
+    images: [
+      {
+        url: 'https://campaign-fe.dev.aura.network/assets/thumb.png',
+      },
+    ],
+    type: 'website',
+    url: 'https://lixi.aura.network',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: `Year of the Dragon’s Li Xi`,
+    description: 'Use Fortune Code to get Li Xi, Share Fortune Code for more Li Xi',
+    images: ['https://campaign-fe.dev.aura.network/assets/thumb.png'],
+  },
 }
- 
+
 export default function Home() {
   return <HomePage />
 }
